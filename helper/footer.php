198bc9c8a689a820e9d989732675c4fe873059a0
Maulana Yusup <lanaysps@gmail.com>
2021-08-06 04:42:17 +0700

    test push

--- helper/footer.php
+++ helper/footer.php
@@ -11,7 +11,7 @@
                 <div class="col-xl-12 col-md-6 col-sm-6 col-12">
                     <ul class="list-inline mb-0 d-flex justify-content-sm-end justify-content-center mr-sm-3 ml-sm-0 mx-3">
                         <li class="list-inline-item  mr-3">
-                            <p class="bottom-footer">&#xA9; 2019 <a target="_blank" href="https://designreset.com/equation">Equation Admin Theme</a></p>
+                            <p class="bottom-footer">&#xA9; <?= date('Y'); ?> <a target="_blank" href="https://lanaysp.com">Lanaysp</a></p>
                         </li>
                         <li class="list-inline-item align-self-center">
                             <div class="scrollTop"><i class="flaticon-up-arrow-fill-1"></i></div>
